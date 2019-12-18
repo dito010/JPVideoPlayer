e5e6cb4e3cf93a0eae39721789d9f46392831e4d
Ethan Lipnik <ethanlipnik@gmail.com>
2019-12-18 16:32:26 -0500

    Changed stuff again for SPM

--- Package.swift
+++ Package.swift
@@ -25,7 +25,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JPVideoPlayer",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources",
+            exclude: [],
+            sources: ["Core"],
+            publicHeadersPath: "Core"),
         .testTarget(
             name: "JPVideoPlayerTests",
             dependencies: ["JPVideoPlayer"]),
